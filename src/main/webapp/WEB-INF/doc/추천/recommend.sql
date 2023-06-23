966987da639a65ff518dbfe80414f6bd104b8c48
ddtc4732 <ddtc4732@gmail.com>
2023-06-23 20:31:18 +0900

    회원 삭제 기능 구현

--- src/main/webapp/WEB-INF/doc/추천/recommend.sql
+++ src/main/webapp/WEB-INF/doc/추천/recommend.sql
@@ -1,6 +1,29 @@
 /**********************************/
 /* Table Name: 추천 */
 /**********************************/
+. 추천 분류(회원 분류 정보): favorite table
+/**********************************/
+/* Table Name: 관심분야 */
+/**********************************/
+
+DROP TABLE favorite CASCADE CONSTRAINTS;
+CREATE TABLE favorite(
+        FAVORITENO                         NUMBER(10)         NOT NULL         PRIMARY KEY,
+        SEQNO                                NUMBER(7)         DEFAULT 1         NULL ,
+        RDATE                                 DATE         NOT NULL,
+        EXHINO                               NUMBER(10)         NULL ,
+        MEMBERNO                          NUMBER(10)         NULL ,
+  FOREIGN KEY (MEMBERNO) REFERENCES MEMBER (MEMBERNO),
+  FOREIGN KEY (EXHINO) REFERENCES EXHI (EXHINO)
+);
+
+COMMENT ON TABLE favorite is '관심분야';
+COMMENT ON COLUMN favorite.FAVORITENO is '관심분야번호';
+COMMENT ON COLUMN favorite.SEQNO is '출력 순서';
+COMMENT ON COLUMN favorite.RDATE is '등록일';
+COMMENT ON COLUMN favorite.EXHINO is '카테고리 번호';
+COMMENT ON COLUMN favorite.MEMBERNO is '회원 번호';
+
 DROP TABLE RECOMMEND;
 
 CREATE TABLE RECOMMEND(
@@ -31,7 +54,7 @@ CREATE SEQUENCE RECOMMEND_SEQ
 
 -- 존재하는 memberno, exhino 등록
 INSERT INTO recommend(recommendno, memberno, exhino, seq, rdate)
-VALUES(RECOMMEND_SEQ.nextval, 1, 15, 1, sysdate);
+VALUES(RECOMMEND_SEQ.nextval, 1, , 1, sysdate);
 
 SELECT recommendno, memberno, exhino, seq, rdate 
 FROM recommend 
@@ -44,3 +67,14 @@ RECOMMENDNO   MEMBERNO     exhiNO        SEQ RDATE
 DELETE FROM recommend;
 DELETE FROM recommend WHERE memberno=1;
 COMMIT;
+
+1. 기존에 추천을 했는지 확인하는 쿼리, 1: 좋아요 클릭함, 0: 아직 좋아요 클릭안함.
+SEELCT COUNT(*) as cnt
+FROM jjim
+WHERE memberno = 1 AND contentsno=1;
+
+2. 좋아요 처리
+INSERT INTO good(goodno, memberno, contentsno, rdate)
+VALUES(good_seq.nextval, 1, 1, sysdata)
+
+
