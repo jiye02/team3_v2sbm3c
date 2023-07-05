b2e35aee968eb881f9ae67744de1cf35d666e71d
ands33 <zip0317@naver.com>
2023-07-05 11:06:45 +0900

    수정 내용 입력

--- src/main/webapp/WEB-INF/doc/찜/jjim.sql
+++ src/main/webapp/WEB-INF/doc/찜/jjim.sql
@@ -54,6 +54,7 @@ commit;
 -- 찜을 했는지 확인후 안했으면 INSERT 실행, 했으면 DELETE 실행
 SELECT COUNT(*) as cnt FROM jjim WHERE memberno=2 AND galleryno=11;
 
+-- 찜 삭제
 DELETE FROM jjim WHERE memberno=2 AND galleryno=1;
 
 -- 관심 상품의 중복 제거
